a8692c97b6f433a6f3611faad2740a21c30109af
Eetu <eetu@iki.fi>
2016-11-26 02:14:54 +0200

    Initial commit of base project

---
+++ Assets/Kitten/Scripts/kittenClicked.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kittenClicked : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    void OnSelect()
+    {
+        Debug.Log("CLICKED!");
+        transform.localPosition += new Vector3(0.0f, 0.1f, 0.0f);
+    }
+    
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
