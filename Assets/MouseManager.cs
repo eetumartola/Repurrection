94087e5fa8d08fc21eeb149388dd15bdd6c5f4b6
Jonathan Granskog <jonathan.granskog@gmail.com>
2016-11-26 23:01:30 +0200

    Mouse Manager

---
+++ Assets/MouseManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+	void OnGUI () {
+        if (Event.current.button == 0)
+        {
+            if (Event.current.type == EventType.MouseDown)
+            {
+                Debug.Log("Clicked!");
+                GameManager.instance.SendMessage("OnClick");
+            }
+        }
+	}
+
+}
