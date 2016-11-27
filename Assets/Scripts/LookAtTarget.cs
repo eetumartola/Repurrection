063440e2a1543e1ce11dbab5464ee41add0ddc53
Jonathan Granskog <jonathan.granskog@gmail.com>
2016-11-27 11:00:55 +0200

    Arrow prefab

---
+++ Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour {
+
+    public GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = new Vector3(0.5f * Mathf.Sin(Time.time), 0.0f, 0.0f);
+        if (target != null) transform.LookAt(target.transform);
+        //else Debug.Log("No target!");
+    }
+
+}
